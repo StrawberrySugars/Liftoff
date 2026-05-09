1d6cc44e146cb69d64562e3522e2fc6feff4d8e6
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-09 17:44:35 -0400

    --base=/my/public/path/ to build in package.json for fixing github pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,8 @@
 	let { children }: { children: Snippet } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 <div class="flex flex-row p-5">
 	{#snippet navItem(name: string, href: string)}
-		<a class="mx-1 rounded-lg border border-black/20 p-2 hover:bg-black/20" href={resolve(href)}
-			>{name}</a
-		>
 	{/snippet}
 	{@render navItem('Foods', '/')}
 	{@render navItem('Beverages', '/beverages')}
