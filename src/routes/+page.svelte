df18ab914a1aede0c9a929df58d0d37db391b58f
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-09 12:50:30 -0400

    created pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
+<!--<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+-->
+
+<div class="navBar">
+    <ul>
+        <li><a id="box" href="\">Foods</a></li>
+        <li><a id="box" href="\beverages">Beverages</a></li>
+    </ul>
+</div>
+
+<h2>Recommended Meal</h2>
+
+<div class="recMeal">
+    <h4>It is recommended for you to have:</h4>
+    <ul>
+        <li>ingredient 1</li>
+        <li>ingredient 2</li>
+    </ul>
+</div>
+
+<h2>Recommended Foods to Grow</h2>
+<h2>List of Foods</h2>
---
+++ src/routes/beverages/+page.svelte
@@ -0,0 +1,10 @@
+<div class="navBar">
+        <ul>
+            <li><a id="box" href="\">Foods</a></li>
+            <li><a id="box" href="\beverages">Beverages</a></li>
+        </ul>
+    </div>
+
+<h2>Recommended Meal</h2>
+<h2>List of Foods</h2>
+<p>beverage page</p>
