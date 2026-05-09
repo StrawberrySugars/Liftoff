b7b414504df1857b81a4a33038d876b7be8f7164
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-09 13:47:48 -0400

    remove script that is for js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,6 @@
 <script lang="ts">
 	// instance-level logic goes here
 </script>
-<!-- 
-<script>
-    // import writable from 'svelte/store';
-
-    // if (document.readyState == 'loading') {
-    //     document.addEventListener('DOMContentLoaded', ready);
-    // } else {
-    //     ready();
-    // }
-
-    ready()
-
-    // // var total = 0;
-    // // var priceAdded = 0;
-    const myData = ['Apple', 'Banana', 'Cherry'];
-    // const listElement = document.getElementById('foodItems');
-
-    // //set up event listeners
-    function ready() {
-        
-    }
-
-
-</script> -->
 
 
 <div class="navBar">
