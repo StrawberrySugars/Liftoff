357cb13a3d5ecbdd5c62b131d88a140b4de04135
clover <me@kittens.fyi>
2026-05-09 19:01:59 +0000

    update beverages

--- src/routes/beverages/+page.svelte
+++ src/routes/beverages/+page.svelte
@@ -1,42 +1,22 @@
 <script lang="ts">
 	import { beverages } from '$lib/beverages';
 	import Popup from '../../components/Popup.svelte';
+	import RecommendedMeals from '../../components/RecommendedMeals.svelte';
 	let shown = $state(false);
 </script>
 
-<div class="navBar">
-	<ul>
-		<li><a id="box" href="\">Foods</a></li>
-		<li><a id="box" href="\beverages">Beverages</a></li>
-	</ul>
-</div>
-
-<h2>Recommended Meal</h2>
-
-<div class="recMeal">
-	<h4>It is recommended for you to have:</h4>
-	<ul>
-		<li>ingredient 1</li>
-		<li>ingredient 2</li>
-	</ul>
-</div>
+<RecommendedMeals />
 
 <h2>List of Beverages</h2>
-<div class="createBeverage">
-    + add a beverage
-</div>
+<div class="createBeverage">+ add a beverage</div>
 <div>
-    {#each $beverages as beverage (beverage.id)}
-        <div class="beverages">
-            <div>
-                {beverage.name} - {beverage.description} 
-            </div>
-            <div class="item-middle">
-                beverage nutrients
-            </div>
-            <div>
-                delete
-            </div>
-        </div>
-    {/each}
+	{#each $beverages as beverage (beverage.id)}
+		<div class="beverages">
+			<div>
+				{beverage.name} - {beverage.description}
+			</div>
+			<div class="item-middle">beverage nutrients</div>
+			<div>delete</div>
+		</div>
+	{/each}
 </div>
