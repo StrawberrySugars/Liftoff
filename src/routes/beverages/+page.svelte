d3e079f48bb7392b91841ce4fff85bc7dc855555
clover <me@kittens.fyi>
2026-05-09 18:04:31 +0000

    add popup

--- src/routes/beverages/+page.svelte
+++ src/routes/beverages/+page.svelte
@@ -1,31 +1,9 @@
 <script lang="ts">
 	import { beverages } from '$lib/stores';
+	import Popup from '../../components/Popup.svelte';
+	let shown = $state(false);
 </script>
 
-<div class="navBar">
-	<ul>
-		<li><a id="box" href="\">Foods</a></li>
-		<li><a id="box" href="\beverages">Beverages</a></li>
-	</ul>
-</div>
-
-<h2>Recommended Meal</h2>
-
-<div class="recMeal">
-	<h4>It is recommended for you to have:</h4>
-	<ul>
-		<li>ingredient 1</li>
-		<li>ingredient 2</li>
-	</ul>
-</div>
-
-<h4>List of Beverages</h4>
-<div>
-    {#each $beverages as beverage (beverage.id)}
-        <div class="beverages">
-        <ul class="beverageList">
-            <li>{beverage.name} - {beverage.description}</li>
-        </ul>
-        </div>
-    {/each}
-</div>
+{#each $beverages as beverage (beverage.id)}
+	<div>{beverage.name} - {beverage.description}</div>
+{/each}
