291aca1ca6d7689e47632f3492a1693bae47f537
clover <me@kittens.fyi>
2026-05-09 17:19:40 +0000

    add script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,22 @@
-<!--<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--->
+<script lang="ts">
+	// instance-level logic goes here
+</script>
 
 <div class="navBar">
-    <ul>
-        <li><a id="box" href="\">Foods</a></li>
-        <li><a id="box" href="\beverages">Beverages</a></li>
-    </ul>
+	<ul>
+		<li><a id="box" href="\">Foods</a></li>
+		<li><a id="box" href="\beverages">Beverages</a></li>
+	</ul>
 </div>
 
 <h2>Recommended Meal</h2>
 
 <div class="recMeal">
-    <h4>It is recommended for you to have:</h4>
-    <ul>
-        <li>ingredient 1</li>
-        <li>ingredient 2</li>
-    </ul>
+	<h4>It is recommended for you to have:</h4>
+	<ul>
+		<li>ingredient 1</li>
+		<li>ingredient 2</li>
+	</ul>
 </div>
 
 <h2>Recommended Foods to Grow</h2>
