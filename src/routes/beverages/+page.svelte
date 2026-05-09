795a8a26bfc82e1a37d2533b3ccf63c72ef1042b
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-09 13:57:28 -0400

    beverages

--- src/routes/beverages/+page.svelte
+++ src/routes/beverages/+page.svelte
@@ -2,6 +2,30 @@
 	import { beverages } from '$lib/stores';
 </script>
 
-{#each $beverages as beverage (beverage.id)}
-	<div>{beverage.name} - {beverage.description}</div>
-{/each}
+<div class="navBar">
+	<ul>
+		<li><a id="box" href="\">Foods</a></li>
+		<li><a id="box" href="\beverages">Beverages</a></li>
+	</ul>
+</div>
+
+<h2>Recommended Meal</h2>
+
+<div class="recMeal">
+	<h4>It is recommended for you to have:</h4>
+	<ul>
+		<li>ingredient 1</li>
+		<li>ingredient 2</li>
+	</ul>
+</div>
+
+<h4>List of Beverages</h4>
+<div>
+    {#each $beverages as beverage (beverage.id)}
+        <div class="beverages">
+        <ul class="beverageList">
+            <li>{beverage.name} - {beverage.description}</li>
+        </ul>
+        </div>
+    {/each}
+</div>
