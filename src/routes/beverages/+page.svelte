5ad948757c9908cf929891a56f5a6029b7376044
StrawberrySugars <jasmine.soetemans@gmail.com>
2026-05-09 23:17:56 -0400

    beverages css images

--- src/routes/beverages/+page.svelte
+++ src/routes/beverages/+page.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
 	import { beverages } from '$lib/beverages';
 	import { MinusIcon, PlusIcon } from '@lucide/svelte';
+	import Popup from '../../components/Popup.svelte';
+	import { plants } from '$lib/planting';
 </script>
 
 <div class="p-5">
 	<h2 class="pb-2 text-lg">List of Beverages</h2>
 	<div class="mt-5">
 		{#each $beverages as beverage, index (beverage.name)}
-			<div class="mb-2 flex rounded-lg border border-gray-300 p-3">
+			<div class="flex h-full flex-col rounded-lg border border-gray-300 transition hover:border-gray-400">
 				<div>
-					<div>
-						{beverage.name}
+                    <img src={beverage.img} alt={beverage.name} class="h-70 w-full rounded-t-lg object-cover">
+					<div class="flex-1 bevTextPad">
+						<h3 class="font-semibold text-gray-900">{beverage.name}</h3>
+						<p class="text-sm text-gray-600">{beverage.ingStr} {beverage.requiredPlants.join(', ')}</p>
 					</div>
-					<div>
-						<img src={beverage.img} alt={beverage.name} />
-					</div>
-					<div class="text-black/60">{beverage.ingStr} {beverage.requiredPlants.join(', ')}</div>
-					<div class="mt-1 text-xs text-black/50"></div>
 				</div>
 				<div class="ml-auto flex items-center gap-3">
 					<button
