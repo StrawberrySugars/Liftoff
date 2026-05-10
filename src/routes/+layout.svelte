a6e3caf8beb286ab6ed0d338383929e04bb3e6ed
clover <me@kittens.fyi>
2026-05-10 00:22:44 +0000

    fix paths

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,22 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { resolve } from '$app/paths';
 	import type { Snippet } from 'svelte';
-	import { base } from '$app/paths';
 
 	let { children }: { children: Snippet } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div class="flex flex-row p-5">
-	{#snippet navItem(name: string, href: string)}
-		<a class="mx-1 rounded-lg border border-black/20 p-2 hover:bg-black/20" {href}>{name}</a>
-	{/snippet}
-	{@render navItem('Foods', '{base}/')}
-	{@render navItem('Beverages', '{base}/beverages')}
-	{@render navItem('Recommended', '{base}/recommended')}
+	<a class="mx-1 rounded-lg border border-black/20 p-2 hover:bg-black/20" href={resolve('/')}
+		>Foods</a
+	>
+	<a
+		class="mx-1 rounded-lg border border-black/20 p-2 hover:bg-black/20"
+		href={resolve('/beverages')}>Beverages</a
+	>
+	<a
+		class="mx-1 rounded-lg border border-black/20 p-2 hover:bg-black/20"
+		href={resolve('/recommended')}>Recommended</a
+	>
 </div>
 {@render children()}
